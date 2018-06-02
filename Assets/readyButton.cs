088223c0663dbf36a4b56e32b01e64e64227f441
Charles CHOLIN <ccholin@e1r3p3.42.fr>
2018-06-03 01:42:31 +0200

    ok

---
+++ Assets/readyButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class readyButton : MonoBehaviour {
+	public InputField value;
+	public GameObject gui;
+	private bool ready = false;
+	private Button btn;
+	// Use this for initialization
+	void Start () {
+		btn = this.GetComponent<Button>();
+		btn.onClick.AddListener(clickReady);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void clickReady()
+	{
+		ready = !ready;
+		if(ready == true){
+			value.readOnly = !value.readOnly;
+		//	JoinGame(int.Parse(value.text));
+		}
+		else
+		{
+			value.readOnly = !value.readOnly;
+		//	JoinGame (-1);
+		}
+		return ;
+	} 
+	void gogame()
+	{
+		gui.SetActive(false);
+		return;
+	}
+}
